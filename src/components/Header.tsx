086983e6a2911629fd2ac4ae27609178c70a6f3c
nafucode <hujiarunez@gmail.com>
2026-04-11 17:57:55 +0800

    feat: Update quote header image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,8 @@
+import Image from 'next/image';
+
 const Header = () => (
-  <div style={{ background: 'linear-gradient(to right, #2d3748, #4a5568)', padding: '1rem' }}>
-    <svg viewBox="0 0 200 25" xmlns="http://www.w3.org/2000/svg">
-      <text x="10" y="15" fill="white" style={{ fontSize: '12px', fontWeight: 'bold' }}>
-        LG
-      </text>
-      <text x="10" y="23" fill="white" style={{ fontSize: '6px', letterSpacing: '0.1em' }}>
-        PRECISION
-      </text>
-      <text x="75" y="23" fill="#A0AEC0" style={{ fontSize: '3px' }}>
-        Precision Elevation, Defining Spaces.
-      </text>
-    </svg>
+  <div>
+    <Image src="/header-banner.png" alt="Company Header" width={800} height={100} style={{ width: '100%', height: 'auto' }} />
   </div>
 );
 
