7f4bbc9e851a0435b7695ab3ac4a2f35efbcf7ff
nafucode <hujiarunez@gmail.com>
2026-05-04 15:27:14 +0800

    feat: add new landing door styles

--- src/data/landingDoorStyles.ts
+++ src/data/landingDoorStyles.ts
@@ -9,4 +9,19 @@ export const landingDoorStyles = [
     name: 'glass-door',
     previewImage: '/landing-door-styles/glass-door.png',
   },
+  {
+    id: 'LD-03',
+    name: '发纹不锈钢',
+    previewImage: '/landing-door-styles/发纹不锈钢Landing-door Standard.png',
+  },
+  {
+    id: 'LD-04',
+    name: '玻璃门',
+    previewImage: '/landing-door-styles/玻璃门glass-door.png',
+  },
+  {
+    id: 'LD-05',
+    name: '货梯-中分双折 4CO',
+    previewImage: '/landing-door-styles/货梯-中分双折 4CO.png',
+  },
 ];
