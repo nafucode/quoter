ee5223b58dd2fb9b4b366abfcd2f0ae401168aa9
nafucode <hujiarunez@gmail.com>
2026-08-24 14:53:37 +0800

    Add landing door material variants

--- src/data/landingDoorStyles.ts
+++ src/data/landingDoorStyles.ts
@@ -5,14 +5,44 @@ export const landingDoorStyles = [
     previewImage: '/landing-door-styles/发纹不锈钢Landing-door Standard.png',
   },
   {
+    id: 'LD-01-MIRROR',
+    name: '镜面不锈钢（中分）',
+    previewImage: '/landing-door-styles/mirror-stainless-center-opening.png',
+  },
+  {
+    id: 'LD-01-ROSE',
+    name: '玫瑰金不锈钢（中分）',
+    previewImage: '/landing-door-styles/rose-gold-center-opening.png',
+  },
+  {
     id: 'LD-02',
     name: '玻璃门',
     previewImage: '/landing-door-styles/玻璃门glass-door.png',
   },
   {
+    id: 'LD-02-MIRROR',
+    name: '镜面不锈钢玻璃门',
+    previewImage: '/landing-door-styles/mirror-stainless-glass-door.png',
+  },
+  {
+    id: 'LD-02-ROSE',
+    name: '玫瑰金玻璃门',
+    previewImage: '/landing-door-styles/rose-gold-glass-door.png',
+  },
+  {
     id: 'LD-03',
     name: '旁开门',
     previewImage: '/landing-door-styles/旁开门.png',
+  },
+  {
+    id: 'LD-03-MIRROR',
+    name: '镜面不锈钢旁开门',
+    previewImage: '/landing-door-styles/mirror-stainless-side-opening.png',
+  },
+  {
+    id: 'LD-03-ROSE',
+    name: '玫瑰金旁开门',
+    previewImage: '/landing-door-styles/rose-gold-side-opening.png',
   },
   {
     id: 'LD-04',
