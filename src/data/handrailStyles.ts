72714a94d12f6944bff7a75e865d4ba2687b7638
nafucode <hujiarunez@gmail.com>
2026-08-24 15:16:38 +0800

    Add handrail material variants

--- src/data/handrailStyles.ts
+++ src/data/handrailStyles.ts
@@ -5,6 +5,16 @@ export const handrailStyles = [
     previewImage: '/handrail-styles/flat-type.png',
   },
   {
+    id: 'HR-01-MIRROR',
+    name: 'flat-type mirror stainless steel',
+    previewImage: '/handrail-styles/flat-type-mirror-stainless.png',
+  },
+  {
+    id: 'HR-01-ROSE',
+    name: 'flat-type rose gold',
+    previewImage: '/handrail-styles/flat-type-rose-gold.png',
+  },
+  {
     id: 'HR-02',
     name: 'round-type(golden)',
     previewImage: '/handrail-styles/round-type(golden).png',
@@ -13,6 +23,16 @@ export const handrailStyles = [
     id: 'HR-03',
     name: 'round-type',
     previewImage: '/handrail-styles/round-type.png',
+  },
+  {
+    id: 'HR-03-MIRROR',
+    name: 'round-type mirror stainless steel',
+    previewImage: '/handrail-styles/round-type-mirror-stainless.png',
+  },
+  {
+    id: 'HR-03-ROSE',
+    name: 'round-type rose gold',
+    previewImage: '/handrail-styles/round-type-rose-gold.png',
   },
   {
     id: 'HR-04',
