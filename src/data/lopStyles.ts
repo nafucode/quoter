838dda0f3a540bc07fb510f7820e6c33ed8d68b9
nafucode <hujiarunez@gmail.com>
2026-09-02 14:36:53 +0800

    Add OZL LOP style options

--- src/data/lopStyles.ts
+++ src/data/lopStyles.ts
@@ -19,4 +19,44 @@ export const lopStyles = [
     name: '黑底白字-标配 2',
     previewImage: '/lop-styles/黑底白字-标配 2.png',
   },
+  {
+    id: 'OZL-W2506',
+    name: 'OZL-W2506',
+    previewImage: '/lop-styles/OZL-W2506.png',
+  },
+  {
+    id: 'OZL-W2507',
+    name: 'OZL-W2507',
+    previewImage: '/lop-styles/OZL-W2507.png',
+  },
+  {
+    id: 'OZL-W2508',
+    name: 'OZL-W2508',
+    previewImage: '/lop-styles/OZL-W2508.png',
+  },
+  {
+    id: 'OZL-W2509',
+    name: 'OZL-W2509',
+    previewImage: '/lop-styles/OZL-W2509.png',
+  },
+  {
+    id: 'OZL-W2510',
+    name: 'OZL-W2510',
+    previewImage: '/lop-styles/OZL-W2510.png',
+  },
+  {
+    id: 'OZL-W2511',
+    name: 'OZL-W2511',
+    previewImage: '/lop-styles/OZL-W2511.png',
+  },
+  {
+    id: 'OZL-W2512',
+    name: 'OZL-W2512',
+    previewImage: '/lop-styles/OZL-W2512.png',
+  },
+  {
+    id: 'OZL-W2513',
+    name: 'OZL-W2513',
+    previewImage: '/lop-styles/OZL-W2513.png',
+  },
 ];
